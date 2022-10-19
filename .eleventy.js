e88d69e37b42fd55b9dcce282498349b47fdaa55
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-10-19 13:58:36 -0400

    Add WebC support

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.addPlugin(pluginWebc, {
+        components: "_includes/webc/*.webc"
+    });
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+
     eleventyConfig.addPassthroughCopy("css" );
+    eleventyConfig.addPassthroughCopy("js" );
 
     eleventyConfig.addShortcode("possum_link", function(slug) {
         return `/possums/${slug}/`
